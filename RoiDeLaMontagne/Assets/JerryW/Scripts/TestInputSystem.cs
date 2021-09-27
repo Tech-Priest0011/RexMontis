9f1affecbc3f14278ede07bf833062a6aa422a50
Jerry Wang <jerrywang0129@hotmail.com>
2021-09-27 00:26:43 -0400

    Debut Input System
    Début du test du nouveau input system pour le mouvement

---
+++ RoiDeLaMontagne/Assets/JerryW/Scripts/TestInputSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputSystem : MonoBehaviour
+{
+   public void Jump()
+    {
+        Debug.Log("Jump");
+    }
+}

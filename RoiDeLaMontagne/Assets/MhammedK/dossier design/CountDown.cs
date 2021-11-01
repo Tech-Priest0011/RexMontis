3108350807361fbb1b0f1ad7a91f2d40c9277da2
Gab629 <70355153+Gab629@users.noreply.github.com>
2021-11-01 08:36:02 -0400

    Ajout du canvas de Mhammed
    -J'ai transféré les fichiers du canvas de Mhammed dans le unity
    -Il comporte un menu avec le score des peronnages et un compte à rebours

---
+++ RoiDeLaMontagne/Assets/MhammedK/dossier design/CountDown.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CountDown : MonoBehaviour 
+{
+    
+    [SerializeField] public Text uiText;
+
+    public int Durer;
+
+    private int tempsRestant;
+
+    private bool Pause;
+
+    private void Start()
+    {
+        Being(Durer);
+    }
+
+    private void Being(int Second)
+    {
+        tempsRestant = Second;
+        StartCoroutine(UpdateTemps());
+    }
+
+    private IEnumerator UpdateTemps()
+    {
+        while(tempsRestant >= 0)
+        {
+            if (!Pause)
+            {
+                uiText.text = $"{tempsRestant / 60:00}:{tempsRestant % 60:00}";
+               
+                tempsRestant--;
+                yield return new WaitForSeconds(1f);
+            }
+            yield return null;
+        }
+        TempsTerminer();
+    }
+
+    private void TempsTerminer()
+    {
+        //End Time , if want Do something
+        print("End");
+    }
+}

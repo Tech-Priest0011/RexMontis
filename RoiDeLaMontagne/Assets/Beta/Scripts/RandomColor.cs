06ed368141c182f2fc7cd6f22799a9518a4a1a8a
Jay <jbcave@hotmail.com>
2021-11-22 21:42:36 -0500

    Couleur random au meep

---
+++ RoiDeLaMontagne/Assets/Beta/Scripts/RandomColor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+    private static Color color1;
+    private static Color color2;
+    private static bool hasColorsAssigned = false;
+    private static string lastColor;
+
+    private void Start()
+    {
+        
+        
+         if (hasColorsAssigned == false)
+         {
+             color1 = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+             color2 = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+             hasColorsAssigned = true;
+         }
+         if (lastColor == "color1")
+         {
+             GetComponent<Renderer>().material.color = color2;
+             lastColor = "color2";
+         }
+         else
+         {
+             GetComponent<Renderer>().material.color = color1;
+             lastColor = "color1";
+         }
+    }
+}
